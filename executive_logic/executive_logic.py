067c0b2c7f772d8a1961f80e401f38ab1c917d23
lu <pena.m.luis@gmail.com>
2022-07-13 00:37:54 -0700

    initial code push

---
+++ executive_logic/executive_logic.py
@@ -0,0 +1,24 @@
+
+# startgame (list_of_configurations):
+- apply configuration 
+- start round 
+    - chose_ctagory()
+- loop until finsihed
+
+return results_of_game # total pooints 
+
+
+def notifySpin():
+    pass 
+
+def getRound():
+    pass 
+
+def whoseTurn():
+    pass 
+
+def selectRandOpponent():
+    pass 
+
+
+
