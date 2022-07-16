fd41357619795ab48e044577b1631fcc6a05006c
robyntorregrosa <rt20@rice.edu>
2022-07-16 18:33:34 -0400

    Client can register, prompted to spin the wheel. Server can respond with result.

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import socket
 import threading
 import logging
 
+
 NUM_PLAYERS = 3
 SRV_IP = 'localhost'
 SRV_PORT = 5555
@@ -12,7 +13,6 @@ class Client:
 
 	# default constructor
 	def __init__(self):
-
 		self.player_id = 1
 		self.whose_turn = 1  # pointer to curent player taking turn
 		self.game_over = False
@@ -21,21 +21,46 @@ class Client:
 		# self.ui = UserInterface()
 
 	def connect_to_game(self, host, port):
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # another TCP socket
-		client.connect((host, port))
-		logging.info("connected to the game")
-		threading.Thread(target= self.handle_connection, args=(client,)).start()  # start threading immediately
+		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # another TCP socket
+		server.connect((host, port))
+		logging.info("Connecting to the game server")
+		threading.Thread(target= self.handle_connection, args=(server,)).start()  # start threading immediately
 
 	def handle_connection(self, client):
 		# check for round over?
 		# self.ExecutiveLogic
 
 		while not self.game_over:
-			turn_message = ""
+			data = client.recv(100)
+			if not data:
+				client.close()
+				break
+			else:
+				server_message = data.decode('utf')
+				parsed_message = server_message.split(",")
+				logging.info("Received message from server: %s", server_message)
 
-			turn_message = "Client registering to server"
-			client.send(turn_message.encode("utf"))  # send message to game server
-			self.game_over = True
+				if parsed_message[0] == "PlayerID":
+					self.player_id = int(parsed_message[1])
+				elif parsed_message[0] == "SPIN_RESULT":
+					logging.info("Received spin result was: " + parsed_message[1])
+					if parsed_message[0] == "SPIN_AGAIN":
+						spin_again = "1"
+						client.send(spin_again.encode("utf"))  # send message to game server
+
+
+				turn_message = ""
+				print("Enter a command for the game server:")
+				print("----- 1) Spin the wheel")
+				print("----- 2) See scores")
+				print("----- 3) List players")
+
+				turn_message = input("Command: ")
+				client.send(str(turn_message).encode("utf"))  # send message to game server
+
+
+
+			# self.game_over = True
 			# if self.whose_turn == self.player_id:  # it's my turn
 			# 	# player takes a turn
 			#
@@ -88,3 +113,5 @@ class Client:
 gameplayer = Client()
 gameplayer.connect_to_game(SRV_IP, SRV_PORT)
 
+# player2 = Client()
+# player2.connect_to_game(SRV_IP, SRV_PORT+1)
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from board.board import Board
 # import score_keeper.score_keeper import ScoreKeeper
 import logging
-logging.basicConfig(level=logging.INFO)
 
+logging.basicConfig(level=logging.INFO)
+# from collections import defaultdict
 
 # https://www.youtube.com/watch?v=s6HOPw_5XuY
 
@@ -12,7 +13,6 @@ import threading
 NUM_PLAYERS = 3
 
 
-
 class GameServer:
 
 	# default constructor
@@ -20,16 +20,21 @@ class GameServer:
 		self.host_ip = srv_ip
 		self.port = srv_port
 
-
 		self.player_id = 1
 		self.whose_turn = 1  # pointer to curent player taking turn
 		self.game_over = False
 
+		self.new_player_id = 1
+		self.players = []  # keep track of players
+
+		self.request_map = {1: "SPIN", 2: "SEE_SCORE", 3: "LIST_PLAYERS"}
+
 		# SETUP SUBSYSTEMS
 		self.board = Board()
-		# self.ExecutiveLogic = ExecutiveLogic()
-		# self.score_keeper = ScoreKeeper()
-		# self.wheel = Wheel()
+
+	# self.ExecutiveLogic = ExecutiveLogic()
+	# self.score_keeper = ScoreKeeper()
+	# self.wheel = Wheel()
 
 	def host_game(self):
 		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # setup server socket
@@ -38,6 +43,13 @@ class GameServer:
 		server.listen(NUM_PLAYERS - 1)  # listen for 2 connections. 3 player game
 
 		client, addr = server.accept()  # next player connection
+
+		player_id_command = 'PlayerID,' + str(self.new_player_id)
+		self.send_command(client, player_id_command)
+
+		# client.send(bytes('Player add client to the players database
+		self.players.append(self.new_player_id)
+		self.new_player_id += 1
 
 		threading.Thread(target=self.handle_connection, args=(client,)).start()  # start threading immediately
 		server.close()  # handle one game
@@ -55,7 +67,7 @@ class GameServer:
 		while not self.game_over:
 			turn_message = ""
 
-			logging.info("Playing the game")
+			logging.info("Handling connections...")
 
 			# if self.whose_turn == self.player_id:  # it's my turn
 			# 	# player takes a turn
@@ -65,28 +77,42 @@ class GameServer:
 			# 	turn_message = "turn over"
 			# 	client.send(turn_message.encode("utf"))  # send message to game server
 
-
 			# else:  # not my turn!
-			data = client.recv(1024)
+			data = client.recv(100)
 			if not data:
 				client.close()
 				break
 			else:
 				client_request = data.decode('utf')
-				parse_request = client_request.split(',')
+				# parse_request = client_request.split(',')
+				print("client request type:", type(client_request), client_request, self.request_map)
+				logging.info("Server received request from user: " + self.request_map[int(client_request)])
 
-				logging.info("Server received request from user: " + client_request)
-					# # command = parse_request[0]
-					# args = parse_request[1:]
-			# 		if parse_request[0] == 'select_category':  # opponent selects category
-			# 			print("opponent" + self.player_id + "selecting the category")
-			# 			pass
-			# 		elif parse_request[0] =='check_answer': # check answer using server score_keeper
-			# 			print("checking score!")
-			# 			pass
-			# 		# elif parse_request[0] == '' # some other command
-			# 		else:
-			# 			self.refresh_display()
+				request_command = self.request_map[int(client_request)]
+
+				if (request_command == "SPIN"):
+					logging.info("Server messaging WHEEL to SPIN ")
+
+					# send message to Wheel!
+					wheel_result = "History"  # TODO: get spin result from wheel
+					logging.info("Server got spin result from WHEEL:" + wheel_result)
+					self.send_command(client, "SPIN_RESULT," + wheel_result)
+			self.game_over = True
+			# # command = parse_request[0]
+			# args = parse_request[1:]
+		# 		if parse_request[0] == 'select_category':  # opponent selects category
+		# 			print("opponent" + self.player_id + "selecting the category")
+		# 			pass
+		# 		elif parse_request[0] =='check_answer': # check answer using server score_keeper
+		# 			print("checking score!")
+		# 			pass
+		# 		# elif parse_request[0] == '' # some other command
+		# 		else:
+		# 			self.refresh_display()
+
+	def send_command(self, client, command):
+		client.send(bytes(command, 'utf-8'))  # command to client
+		# client.close()  # close connection
 
 	def refresh_score(self, score):
 		"""
