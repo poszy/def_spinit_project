8f9a8572f61fd9f3c8c744a04faa9a3c08ca5089
robyntorregrosa <rt20@rice.edu>
2022-07-18 22:53:02 -0400

    add return values for interfaces- execLogic

--- interface_demo.py
+++ interface_demo.py
@@ -1,6 +1,8 @@
 from server import GameServer
 import logging
 from collections import defaultdict
+from ui.user_interface import UserInterface
+
 class InterfaceDemo(GameServer):
 
     def demo_all_interfaces(self):
@@ -33,10 +35,23 @@ class InterfaceDemo(GameServer):
     def __demo_wheel(self):
         logging.info("\n\nPinging Wheel Interfaces")
 
-        logging.info("get_spint_result(): %s", self.wheel.get_spint_result())
+        logging.info("get_spin_result(): %s", self.wheel.get_spin_result())
+        logging.info('update_categories(["String Theory", "Politics"]): %s', self.wheel.update_categories(["String Theory", "Politics"]))
+
 
     def __demo_user_interface(self):
         logging.info("\n\nPinging UI Interfaces")
+        user_interface = UserInterface()
+
+        scores_dict = defaultdict(int)
+        tokens_dict = defaultdict(int)
+        add_items = [("Wil", 1000, 1), ("Luis", 9999, 3), ("Robyn", 1024, 0)]
+        for player, pts, tokens in add_items:
+            scores_dict[player] = pts
+            tokens_dict[player] = tokens
+        args = {"question": "What is a byte?", "options": ["A", "B", "C", "D"], "points": 100}
+        logging.info("update_score(scores, tokens): %s", user_interface.update_score(scores_dict, tokens_dict))
+        logging.info('update_display(displayItems): %s', user_interface.update_display(args))
 
     def __demo_executive_logic(self):
         logging.info("\n\nPinging Executive Logic Interfaces")
@@ -44,6 +59,7 @@ class InterfaceDemo(GameServer):
         logging.info("notifySpin(): %s", self.executive_logic.notifySpin())
         logging.info("getRound(): %s", self.executive_logic.getRound())
         logging.info("whoseTurn(): %s", self.executive_logic.whoseTurn())
+        logging.info("updateTurn(): %s", self.executive_logic.updateTurn())
         logging.info("selectRandOpponent(): %s", self.executive_logic.selectRandOpponent())
 
 
--- ui/user_interface.py
+++ ui/user_interface.py
@@ -1,3 +1,9 @@
 class UserInterface:
     def __init__(self):
         pass
+
+    def update_score(self, scores_dict, tokens_dict):
+        return scores_dict, tokens_dict
+
+    def update_display(self, args):
+        return args
