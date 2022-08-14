d69dabeb04e8e234a69e9aa0ca69ced6c1a0b482
Wilson-Orlando <wilaorlando@gmail.com>
2022-08-13 21:33:01 -0400

    Implemented multiplayer. Confirmed behavior through several turns of gameplay with two players.

--- executive_logic/executive_logic.py
+++ executive_logic/executive_logic.py
@@ -15,7 +15,7 @@ from tkinter.messagebox import showinfo
 MAX_SPINS = 50
 NUM_ROUNDS = 2
 MAX_NUM_PLAYERS = 3
-DEFAULT_QUESTIONS_FILE = 'board/JArchive-questions.csv'  # need to use relative path from current working directory of main.py!
+DEFAULT_QUESTIONS_FILE = './JArchive-questions.csv'  # need to use relative path from current working directory of main.py!
 
 
 class ExecutiveLogic:
@@ -36,6 +36,7 @@ class ExecutiveLogic:
 
         self.query_status = QueryStatus.STANDBY
         self.query_response = Message(MessageType.EMPTY, [])
+        self.waiting_on_player_id = None
 
     def run_game(self):
         """
@@ -59,14 +60,11 @@ class ExecutiveLogic:
         """
         self.num_spins = 0
         curr_player_id = None
-        # TODO: At start of round, send Jeopardy board to server (then client) with UPDATE_BOARD MessageType
         # End round when spins >= 50 or no available questions
         while self.num_spins < MAX_SPINS and self.board.get_available_categories(round_num):
             curr_player_id = self.__next_player(curr_player_id)
-            # self.__query_server(MessageType.UPDATE_BOARD, [self.board]) TODO
-            self.__query_server(MessageType.SPIN, [
-                curr_player_id])  # Notify player that it's their turn,  ask them to push a button to spin the wheel, and wait for their response
-            self.__execute_turn(curr_player_id, round_num)
+            logging.info(f"Starting player %d's turn", curr_player_id)
+            self.__execute_turn(curr_player_id, round_num)  # Start turn
 
     def __execute_turn(self, curr_player_id, round_num):
         """
@@ -75,12 +73,13 @@ class ExecutiveLogic:
         :return: void
         """
 
-        # Spin wheel
+        # Wait for player to confirm spin
+        self.__query_server(curr_player_id, MessageType.SPIN, [])
+
+        # Get result from wheel spin
         if self.num_spins >= MAX_SPINS:
             return
-        wheel_result = self.__spin_wheel()
-        # self.__query_server(MessageType.SPIN_RESULT, [wheel_result])
-        # TODO: Implement, so that user can see what sector they spun
+        wheel_result = self.__spin_wheel(curr_player_id)
 
         # If result is a Jeopardy category, call execute_category
         if self.wheel.is_jeopardy_category(wheel_result):
@@ -93,37 +92,35 @@ class ExecutiveLogic:
                 if is_correct:  # If playeris correct, they spin again
                     self.__execute_turn(curr_player_id, round_num)  # Spin again
 
-
-
         # If result is another wheel sector, handle logic
         elif wheel_result == Sector.LOSE_TURN:
             # TODO: Rewrite to prompt player to use token. Currently just uses a token if it's available.
             if self.score_keeper.use_token(curr_player_id)[0]:  # Use token if available
-                self.__update_scores_tokens_spins()
+                self.__update_scores_tokens_spins(curr_player_id)
                 self.__execute_turn(curr_player_id, round_num)  # Spin again
             else:  # Otherwise, end player turn
                 return
 
         elif wheel_result == Sector.FREE_TURN:
             self.score_keeper.add_token(curr_player_id)  # Give the player a token
-            self.__update_scores_tokens_spins()
+            self.__update_scores_tokens_spins(curr_player_id)
             self.__execute_turn(curr_player_id, round_num)  # Spin
 
         elif wheel_result == Sector.BANKRUPT:
             self.score_keeper.bankrupt(curr_player_id)
-            self.__update_scores_tokens_spins()
+            self.__update_scores_tokens_spins(curr_player_id)
             return  # End player turn
 
         elif wheel_result == Sector.PLAYERS_CHOICE:
-            open_categories = self.board.get_available_categories()
-            self.__query_server(MessageType.PLAYERS_CHOICE, [curr_player_id, open_categories])
+            open_categories = self.board.get_available_categories(round_num)
+            self.__query_server(curr_player_id, MessageType.PLAYERS_CHOICE, [open_categories])
             _, [chosen_category] = self.query_response.code, self.query_response.args
             self.__execute_category(chosen_category, curr_player_id, round_num)
 
         elif wheel_result == Sector.OPPONENTS_CHOICE:
             open_categories = self.board.get_available_categories(round_num)
             # TODO: Have the exec logic pick a random opponent that is not curr_player_id
-            self.__query_server(MessageType.OPPONENTS_CHOICE, [curr_player_id, open_categories])
+            self.__query_server(curr_player_id, MessageType.OPPONENTS_CHOICE, [open_categories]) # TODO: Not curr_player_id
             _, [chosen_category] = self.query_response.code, self.query_response.args
             self.__execute_category(chosen_category, curr_player_id, round_num)
 
@@ -133,19 +130,19 @@ class ExecutiveLogic:
         else:
             raise Exception("Wheel result '" + wheel_result + "' is not a valid wheel category!")
 
-    def __execute_category(self, jeopardy_category, player_id, round_num):
+    def __execute_category(self, jeopardy_category, curr_player_id, round_num):
         """
         Controls the flow of a Jeopardy question. Called when a "category" sector is the result of a wheel spin.
         :param jeopardy_category: Name of jeopardy category
         :return: is_correct (bool): True if the player's answer was correct, False otherwise
         """
         tile = self.board.get_tile(jeopardy_category, round_num)  # Get tile from board
-        self.__query_server(MessageType.JEOPARDY_QUESTION,
-                            [player_id, jeopardy_category, tile])  # Display tile to user, wait for response
+        self.__query_server(curr_player_id, MessageType.JEOPARDY_QUESTION,
+                            [jeopardy_category, tile])  # Display tile to user, wait for response
         _, [user_answer] = self.query_response.code, self.query_response.args
         is_correct, points = tile.check_answer(user_answer)
-        self.score_keeper.update_score(player_id, is_correct, points)
-        self.__update_scores_tokens_spins()
+        self.score_keeper.update_score(curr_player_id, is_correct, points)
+        self.__update_scores_tokens_spins(curr_player_id)
         return is_correct
 
     def __end_game(self):
@@ -154,7 +151,7 @@ class ExecutiveLogic:
         :return: void
         """
         winner_id = self.score_keeper.determine_winner()
-        self.__query_server(MessageType.END_GAME, [winner_id])  # Tell server who won, and to end game
+        self.__notify_all_players(MessageType.END_GAME, [winner_id])  # Tell server who won, and to end game
         self.is_game_running = False
 
     def __next_player(self, curr_player_id):
@@ -166,19 +163,19 @@ class ExecutiveLogic:
         if curr_player_id is None:
             return 0
         else:
-            return (curr_player_id + 1) % len(self.game_server.players)
+            return (curr_player_id + 1) % self.game_server.num_players
 
-    def __spin_wheel(self):
+    def __spin_wheel(self, curr_player_id):
         """
         Called whenever the wheel must be spun. Increments spin counter and returns spin result.
         :return: Result of wheel spin
         """
         self.num_spins += 1
         spin_result = self.wheel.get_spin_result()
-        self.__query_server(MessageType.SPIN_RESULT, [spin_result])
+        self.__query_server(curr_player_id, MessageType.SPIN_RESULT, [spin_result])
         return spin_result
 
-    def __query_server(self, command: MessageType, args: list):
+    def __query_server(self, player_id: int, command: MessageType, args: list):
         """
         Called whenever information must be obtained from the server (e.g. asking the user for something).
         Stalls until a response is received. Will only accept a response of the type that was requested.
@@ -191,10 +188,13 @@ class ExecutiveLogic:
 
         self.query_response = Message(MessageType.EMPTY, [])  # Clear server's previous response
         self.server_message = Message(command, args)  # Store the query in self.server_message for server to read
+        self.waiting_on_player_id = player_id
+
         self.query_status = QueryStatus.SERVER_TO_CLIENT  # Tell server we want to query client
 
         while self.query_status is not QueryStatus.STANDBY:  # Wait for a response from the server
             pass
+
         logging.info(f"QUERY_SERVER RESPONSE CODE: {self.query_response.code}")
 
     def store_query(self, command: MessageType, args: list):
@@ -206,39 +206,26 @@ class ExecutiveLogic:
         """
         self.query_response = Message(command, args)  # Store server response in self.query_response
 
-    def __update_scores_tokens_spins(self):
+    def __update_scores_tokens_spins(self, curr_player_id):
         """
         Called after a game value is updated that should be reflected in the UI display.
         Sends server the current player scores, player tokens, and number of spins remaining.
         :return: void
         """
-        self.__query_server(MessageType.UPDATE_SCORES,
+        self.__query_server(curr_player_id, MessageType.UPDATE_SCORES,
                             [self.score_keeper.get_scores(), self.score_keeper.get_tokens(),
                              MAX_SPINS - self.num_spins])
 
-    ########
-    # TODO: All methods below this point may be deprecated?
-    ########
-    def notify_spin(self):
-        self.num_spins += 1
-        return self.num_spins
-
-    def whose_turn(self,
-                   curr_player_id):  # TODO: This method may require changing curr_player_id to a class variable, or passing the information down from Executive Logic to the class that needs it. Revisit later.
+    def __notify_all_players(self, command: MessageType, args: list):
         """
-        Returns name of the player who is currently taking their turn, or None if no one is currently taking a turn.
-        :return: (string) Player name
+        Called to send a message to all players.
+        Used to update all players on other players' spins, score updates, etc.
+        :return: void
         """
-        if curr_player_id is None:
-            return None
-        else:
-            return self.game_server.players[self.curr_player_id]
+        for player_id in self.game_server.players:
+            self.__query_server(player_id, command, args)
 
-    def update_turn(self):
-        # return the next player
-        return "Luis"
-
-    def select_rand_opponent(self):
+    def __select_rand_opponent(self):  # TODO: Implement
         return "Opponent"
 
 
